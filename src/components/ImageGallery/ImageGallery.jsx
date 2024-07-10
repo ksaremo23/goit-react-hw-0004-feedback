78f8aeebb05501ba599a451395ec227dda012c4b
Leicsha Karen Saremo <120180371+ksaremo23@users.noreply.github.com>
2024-07-11 01:21:38 +0800

    2nd

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import css from './ImageGallery.module.css';
+
+
+const ImageGallery = ({ images }) => {
+  return (
+    <ul className={`${css.gallery} js-gallery`}>
+      {images.map(image => (
+        <ImageGalleryItem key={image.id} image={image} />
+      ))}
+    </ul>
+      
+    );
+  };
+  
+  ImageGallery.propTypes = {
+    images: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+      })
+    ).isRequired,
+  };
+
+export default ImageGallery;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ImagesProvider } from 'context/ImagesProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ImagesProvider>
+      <App />
+    </ImagesProvider>
   </React.StrictMode>
 );
